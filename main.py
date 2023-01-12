b2493362f998aff223d64cc8c4ab24011adb547b
lbparis <parisblucas@gmail.com>
2023-01-11 21:33:15 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+first_name = (str.title(input("Give me a name: ")))
+location = input("A location: ")
+whole_number = input("A number: ")
+plural_noun = input("A thing: ")
+
+
+
+
+
+print("{} went to {} to buy {} different types of {}. Unfortunately, {} had not watched the news to see that {} "
+      "had been banned from {} so there weren't any there! Unbelievable!".format(first_name, location, whole_number,
+                                                                plural_noun, first_name, plural_noun, location
+                                                                                 ))
